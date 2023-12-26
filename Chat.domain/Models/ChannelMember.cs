3c40347751484a2c022cf5cdff9cf14bb9a47bf0
barbara3210 <bivkovic@pmfst.hr>
2023-12-26 17:39:38 +0100

    chat domain models

--- Chat.domain/Models/ChannelMember.cs
+++ Chat.domain/Models/ChannelMember.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 
 namespace Chat.domain.Models
 {
-    public class User
+    public class ChannelMember
     {
+        public int ChannelId { get; set; }
         public int UserId { get; set; }
-        public string Email { get; set; }
-        public int PasswordHash { get; set; }
-        public bool IsAdmin { get; set; }
     }
 }
--- Chat.domain/Models/Message.cs
+++ Chat.domain/Models/Message.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Chat.domain.Models
 {
-    internal class Message
+    public class Message
     {
+        public int MessageId { get; set; }
+        public int ChannelId { get; set; }
+        public int SenderUserId { get; set; }
+        public int RecipientUserId { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime Time { get; set; }
+
     }
 }
