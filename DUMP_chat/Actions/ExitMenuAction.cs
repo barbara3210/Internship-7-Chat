477d3f3d0e8e9ba72662b8298484012983ec602b
barbara3210 <bivkovic@pmfst.hr>
2023-12-28 17:17:10 +0100

    presentation actions

---
+++ DUMP_chat/Actions/ExitMenuAction.cs
@@ -0,0 +1,19 @@
+﻿using Chat.presentation.Abstractions;
+
+
+namespace Chat.presentation.Actions
+{
+    public class ExitMenuAction : IAction
+    {
+        public int MenuIndex { get; set; }
+        public string Name { get; set; } = "Exit";
+
+        public ExitMenuAction()
+        {
+        }
+        public void Open()
+        {
+        }
+    }
+
+}
